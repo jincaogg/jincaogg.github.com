1508a882d2f2011196f6e084b57b4f14c9b7a50c
jingle <jincaogg@gmail.com>
2013-10-07 09:45:07 +0800

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
-../blog/bin/blog.py
-
 git rm *.htm
 git rm -r page
 git rm -r post
 git rm -r cat
 git rm -r tag 
+rm -r ../blog/public/* 
+
+../blog/bin/blog.py
 cp -R ../blog/public/* ./
+
 git add .
 git commit -m 'update'
 git push
